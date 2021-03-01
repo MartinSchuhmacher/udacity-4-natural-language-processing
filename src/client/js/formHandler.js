79d99f73a2a8ae708b8e7432e3a3ae0a44cd6006
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-03-01 09:46:02 +0100

    error handling wrong url format

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -22,21 +22,29 @@ function handleSubmit(event) {
     // check what text was put into the form field
     const formText = document.getElementById('url').value;
     const checkResult = Client.checkForUrl(formText);
+    // for debuging only
     console.log("Result: " + checkResult);
     console.log("::: Starting Request :::");
-    fetch('http://localhost:8081/article', { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            value: formText
+
+    // if check confirms valid URL
+    if (checkResult) {
+        fetch('http://localhost:8081/article', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                value: formText
+            })
+            }
+        )
+        .then(function() {
+            Client.getData('/all');
         })
-        }
-    )
-    .then(function() {
-        Client.getData('/all');
-    })
+    }
+    else {
+        alert("<Please enter a valid URL starting with http/https>");
+    }
 };
     
 
