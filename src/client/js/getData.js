e292a968b4c2f74d72379ddfad6f7fecca7055ac
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-03-03 15:35:53 +0100

    show results to user

--- src/client/js/getData.js
+++ src/client/js/getData.js
@@ -20,6 +20,12 @@ async function getData(url='') {
     const response = await fetch(url);
     try {
         const allData = await response.json();
+        document.getElementById('summary').innerHTML = `URL "${allData.url}" analyzed by model "${allData.model}" gives the following results.`;
+        document.getElementById('score-tag').innerHTML = `Polarity score tag: ${allData.score}`;
+        document.getElementById('polarity-between').innerHTML = `Polarity between text segments: ${allData.polarityBetween.toLowerCase()}`;
+        document.getElementById('subjectivity').innerHTML = `Subjectivity: ${allData.subjectivity.toLowerCase()}`;
+        document.getElementById('confidence').innerHTML = `Confidence (between 0 and 100): ${allData.confidence}`;
+        document.getElementById('irony').innerHTML = `Irony: ${allData.irony.toLowerCase()}`;
         console.log(allData);
     }
     catch(error) {
