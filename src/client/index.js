5717d8a0fcb39f8a3bb1f5b59d1a542ef877d762
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-03-06 18:35:06 +0100

    adding scss style

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import {handleSubmit} from './js/formHandler.js';
 import {checkForUrl} from './js/checkForUrl.js';
 import {getData} from './js/getData.js';
-// import './styles/base.scss';
+import './styles/style.scss';
 
 //alert("I EXIST");
 
